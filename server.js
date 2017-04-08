df702165d4e611941d7a6eec7b7f93be784db34d
subhan <khansubhan20@yahoo.com>
2017-04-08 10:25:14 +0530

    Add cors library

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express')
 var request = require('request')
+var cors = require('cors')
 
 require('dotenv').config()
 var app = express()
 
-app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
+app.use(cors());
 
 app.get('/api/weather', function(req, res) {
 	var lat = req.query.lat
